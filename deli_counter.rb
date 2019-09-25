3e843a0e433b35318c65a7f8a30007bab669ce18
Hannah Harrington <hannahharrington@outlook.com>
2019-09-25 13:02:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,6 +9,9 @@ else
       counter +1
     end
     puts "#{current_line}"
- 
+   array.each.with_index(1) do |name,index|
+     current_line += "#{index}. #{name}"
+   end
+   puts "#{current_line}" 
  end
 end 
