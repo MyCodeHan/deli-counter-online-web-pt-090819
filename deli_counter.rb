ca052ec6cac1bd9544bacb48d8ba408f9854cd37
Hannah Harrington <hannahharrington@outlook.com>
2019-09-25 13:03:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 def line(array)
     if array.empty?
   puts "The line is currently empty."
+
 else
   current_line = "The line a currently:"
     counter = 1
@@ -9,6 +10,7 @@ else
       counter +1
     end
     puts "#{current_line}"
+   
    array.each.with_index(1) do |name,index|
      current_line += "#{index}. #{name}"
    end
