acc4892f4431a6166b99a983cfeba0f2821136b0
Hannah Harrington <hannahharrington@outlook.com>
2019-09-25 13:17:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,7 +26,9 @@ def now_serving(array)
   if array.empty?
   puts "There is nobody waiting to be served!"
 else
-  array.shift
-  array
+  arr = "#{name}"
+  puts "Currently serving #{array.shift}"
  end
+ 
+ 
 end
