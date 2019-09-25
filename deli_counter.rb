afc300e35660f54a53bf06d6f6967615272b3b1c
Hannah Harrington <hannahharrington@outlook.com>
2019-09-25 12:50:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(array)
+    if array.empty?
+  puts "The line is currently empty."
+else
+  current_line "this line a currently:"
+  counter = 1  
+  array.each do| name| 
+  current_line += " #{counter}. #{name}"
+  counter += 1 
+end
+  puts "#{current_line}"
+  puts ("The line is currently: 1. Logan 2. Avi 3. Spencer")
+ end
+end 
